695911f41c3e04e2a43bb44c68554db2132b4419
Ionut <laurentiuionut39@gmail.com>
2020-07-31 08:40:08 +1100

    Structure de base de l'appication web.
    Creation de projets Django et React au sein du dossier web_application.

--- django_rasbery_server/wifi_cerberus/file_crater/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.urls import path, include
-from . import views
-
-app_name = 'polls'
-urlpatterns = [
-    path('captures/', views.captures_list, name='captures_list'),
-    path('<int:num>/', views.getFile),
-] 
